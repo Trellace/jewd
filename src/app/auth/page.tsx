d1cf4e0e819060dd125af17e23bd0b3c5cf0dc3a
Trellace <dwheeler146@outlook.com>
2025-08-15 21:23:56 +1000

    updates

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -6,10 +6,12 @@ import Link from "next/link";
 // THIS IS THE AUTH PAGE - /auth route
 export default function Page() {
   return (
-    <div className="h-screen w-screen p-4">
+    <div className="h-screen w-screen p-4 flex items-center justify-center">
         <Link href={'/'}>
             <Button variant={'ghost'} className="absolute left-4 top-4"><ArrowLeft/></Button>
         </Link>
+
+        <h1 className="font-bold text-2xl">Auth page placeholder</h1>
 
     </div>
   );
